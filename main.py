494258c58bc1f0b3695ec88513d0449aeca362f7
Vuyani2 <vuyanikunelisi@gmail.com>
2021-07-30 14:40:39 +0200

    changes

--- main.py
+++ main.py
@@ -14,24 +14,27 @@ l1.grid(row=2, column=0)
 E1 = Entry(l1, state='disable')
 E1.grid(row=4, column=0)
 
+
 def Cel_Active():
     E2.configure(state='disable')
     E1.configure(state='normal')
 
+
 btn_active=Button(window,text='Activate -Celcius to Fahrenheit', command=Cel_Active)
 btn_active.grid(row=6, column=0)
-
 l2 = LabelFrame(window, text='Fahrenheit to Celcius', padx=20, pady=20)
 l2.grid(row=2, column=5)
 E2 = Entry(l2, state='disable')
 E2.grid(row=4, column=5)
 
+
 def Far_Active():
     E1.configure(state='disable')
     E2.configure(state='normal')
+
+
 btn_active1=Button(window,text='Activate -Fahrenheit to Celcius', command=Far_Active)
 btn_active1.grid(row=6, column=5)
-
 
 
 def exit():
@@ -45,6 +48,7 @@ def exit():
 exit_btn = Button(text = "Quit", command=exit)
 exit_btn.grid(row=9, column=6)
 
+
 def convert_C():
     try:
         if E1:
@@ -53,7 +57,6 @@ def convert_C():
             result_entry.config(state="normal")
             result_entry.delete(0, END)
             result_entry.insert(0, str(fahrenheit))
-            result_entry.config(state="readonly")
     except ValueError as ex:
         messagebox.showerror("Error", "Please type a number as temperature")
 
@@ -66,9 +69,9 @@ def convert_f():
             result_entry.config(state="normal")
             result_entry.delete(0, END)
             result_entry.insert(0, celcius)
-            result_entry.config(state="readonly")
     except ValueError as ex:
         messagebox.showerror("Error", "Please type a number as temperature")
+
 
 result_bnt = Button(window, text = 'Convert C-F', command=convert_C)
 result_bnt.grid(row=7, column=2)
@@ -82,12 +85,15 @@ result_entry.grid(row=7, column=3)
 
 
 def Clear():
-    E1.configure(state="disable")
-    E2.configure(state="disable")
     E1.delete(0, END)
     E2.delete(0, END)
-    result_entry.delete(0,END)
-Clear_btn=Button(window, text='Clear',command=Clear)
+    result_entry.delete(0, END)
+    E1.configure(state="disable")
+    E2.configure(state="disable")
+    result_entry.config(state="readonly")
+
+
+Clear_btn=Button(window, text='Clear', command=Clear)
 Clear_btn.grid(row=8, column=6)
 
 window.mainloop()
